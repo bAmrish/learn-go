5be6da28c89ffd4c83a1c081e00737643e9ea202
Amrish Bharatiya <bamrish@gmail.com>
2023-09-30 23:30:01 +0530

    Slightly refactored the random-distribution example

--- exercises/01-random-distribution.go
+++ exercises/01-random-distribution.go
@@ -1,4 +1,4 @@
-// Go has `math/rand` packages that provide utility functions to genrate random number.
+// Go has `math/rand` packages that provide utility functions to generate random number.
 // This program is an attempt to capture the distribution of those random numbers over different ranges.
 package main
 
@@ -16,14 +16,19 @@ func main() {
 
 func distribution(limit int) {
 	dist := map[int]int{}
-	iteraions := limit * 1000
+	iterations := limit * 1000
 
-	for i := 0; i < iteraions; i++ {
+	for i := 0; i < iterations; i++ {
 		r := rand.Intn(limit)
-		_, exists := dist[r]
-		if !exists {
-			dist[r] = 0
-		}
+		// _, exists := dist[r]
+		// if !exists {
+		// 	dist[r] = 0
+		// }
+		// If a key does not exists in a map the zero-value of
+		// value type is returned. In this case the value type is int
+		// with the zero-value of 0, so when we set the key for the first
+		// time we are just setting it to 1.
+		// So the above check is not required
 		dist[r] += 1
 	}
 
